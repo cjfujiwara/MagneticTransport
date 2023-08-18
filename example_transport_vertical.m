e37af7e8b3d778aaf28c8082ffbe566e955bc6ae
thywissen-lab-uoft <52182330+thywissen-lab-uoft@users.noreply.github.com>
2023-08-18 11:54:35 -0400

    Update example_transport_vertical.m

--- example_transport_vertical.m
+++ example_transport_vertical.m
@@ -579,207 +579,100 @@ xlabel('position (mm)');
 ylabel('current (A)');
 xlim(1e3*[zd z_final]);
 ylim([-50 70]);
+
+%% Final Output
+data = struct;
+data.z0a = z_0a;
+data.z0a = z_0a;
+
+% Raw
+Z; %Position in meters 1xN
+G; % Gradient in G/cm Nx6
+B; % Field in G Nx6
+
+% 0 to a
+z_0a;
+i1_0a;
+i2_0a;
+i3_0a;
+
+% a to b
+z_ab;
+i1_ab;
+i2_ab;
+i3_ab;
+i4_ab;
+
+% b to c
+z_bc;
+i2_bc;
+i3_bc;
+i4_bc;
+i5_bc;
+
+% c to d
+z_cd;
+i3_cd;
+i4_cd;
+i5_cd;
+i6_cd;
+
+% d to fina
+z_dfinal;
+i4_dfinal;
+i5_dfinal;
+i6_dfinal;
+
+% Combine them
+i1 = [i1_0a i1_ab zeros(1,length(z_bc)) zeros(1,length(z_cd)) zeros(1,length(z_dfinal))];
+i2 = [i2_0a i2_ab i2_bc zeros(1,length(z_cd)) zeros(1,length(z_dfinal))];
+i3 = [i3_0a i3_ab i3_bc i3_cd zeros(1,length(z_dfinal))];
+i4 = [zeros(1,length(z_0a)) i4_ab i4_bc i4_cd i4_dfinal];
+i5 = [zeros(1,length(z_0a)) zeros(1,length(z_ab)) i5_bc i5_cd i5_dfinal];
+i6 = [zeros(1,length(z_0a)) zeros(1,length(z_ab)) zeros(1,length(z_bc)) i6_cd i6_dfinal];
+zz = [z_0a z_ab z_bc z_cd z_dfinal];
 %%
 
-
-%{
-
-%% Zone 1 Solution OLD
-zvec = linspace(za,zb,100);
-
-c1 = coils(1).Coil;
-c2 = coils(2).Coil;
-c3 = coils(3).Coil;
-c4 = coils(4).Coil;
-dL  = 0.0001;
-
-% Gradient and field value
-G = 100;
-B = 0;
-b = [B; G];
-
-% Field at BC1
-[~,~,Bz1]=fieldCoil_3D(0,0,za,c1);      % Coil 1
-[~,~,Bz2]=fieldCoil_3D(0,0,za,c2);      % Coil 2
-[~,~,Bz3]=fieldCoil_3D(0,0,za,c3);      % Coil 3
-[~,~,Bz4]=fieldCoil_3D(0,0,za,c4);      % Coil 4
-Bz1 = Bz1*1e4;
-Bz2 = Bz2*1e4;
-Bz3 = Bz3*1e4;         
-Bz4 = Bz4*1e4;
-
-% Gradient at BC1
-[~,~,Bz1p]=fieldCoil_3D(0,0,za+dL,c1);
-[~,~,Bz2p]=fieldCoil_3D(0,0,za+dL,c2);
-[~,~,Bz3p]=fieldCoil_3D(0,0,za+dL,c3);
-[~,~,Bz4p]=fieldCoil_3D(0,0,za+dL,c4);
-[~,~,Bz1n]=fieldCoil_3D(0,0,za-dL,c1);
-[~,~,Bz2n]=fieldCoil_3D(0,0,za-dL,c2);
-[~,~,Bz3n]=fieldCoil_3D(0,0,za-dL,c3);
-[~,~,Bz4n]=fieldCoil_3D(0,0,za-dL,c4);
-Gz1 = (Bz1p-Bz1n)/(2*dL);Gz1 = Gz1*1e2;
-Gz2 = (Bz2p-Bz2n)/(2*dL);Gz2 = Gz2*1e2;
-Gz3 = (Bz3p-Bz3n)/(2*dL);Gz3 = Gz3*1e2;  
-Gz4 = (Bz4p-Bz4n)/(2*dL);Gz4 = Gz4*1e2;
-
-M1 = [Bz1 Bz3; Gz1 Gz3];
-i1 = M1\b;
-i1 = [i1(1) 0  i1(2) 0];
-
-
-% Field at BC1
-[~,~,Bz1]=fieldCoil_3D(0,0,zb,c1);      % Coil 1
-[~,~,Bz2]=fieldCoil_3D(0,0,zb,c2);      % Coil 2
-[~,~,Bz3]=fieldCoil_3D(0,0,zb,c3);      % Coil 3
-[~,~,Bz4]=fieldCoil_3D(0,0,zb,c4);      % Coil 4
-Bz1 = Bz1*1e4;
-Bz2 = Bz2*1e4;
-Bz3 = Bz3*1e4;         
-Bz4 = Bz4*1e4;
-
-% Gradient at BC1
-[~,~,Bz1p]=fieldCoil_3D(0,0,zb+dL,c1);
-[~,~,Bz2p]=fieldCoil_3D(0,0,zb+dL,c2);
-[~,~,Bz3p]=fieldCoil_3D(0,0,zb+dL,c3);
-[~,~,Bz4p]=fieldCoil_3D(0,0,zb+dL,c4);
-[~,~,Bz1n]=fieldCoil_3D(0,0,zb-dL,c1);
-[~,~,Bz2n]=fieldCoil_3D(0,0,zb-dL,c2);
-[~,~,Bz3n]=fieldCoil_3D(0,0,zb-dL,c3);
-[~,~,Bz4n]=fieldCoil_3D(0,0,zb-dL,c4);
-Gz1 = (Bz1p-Bz1n)/(2*dL);Gz1 = Gz1*1e2;
-Gz2 = (Bz2p-Bz2n)/(2*dL);Gz2 = Gz2*1e2;
-Gz3 = (Bz3p-Bz3n)/(2*dL);Gz3 = Gz3*1e2;  
-Gz4 = (Bz4p-Bz4n)/(2*dL);Gz4 = Gz4*1e2;
-
-M2 = [Bz2 Bz4; Gz2 Gz4];
-i2 = M2\b;
-i2 = [0 i2(1) 0  i2(2)];
-
-%% Calculate all fields
-Bz = zeros(length(zvec),4);
-Gz = zeros(length(zvec),4);
-curr_lin = zeros(length(zvec),4);
-
-for jj=1:length(zvec)
-    z0 = zvec(jj);
-
-    % Field Values
-    [~,~,Bz1]=fieldCoil_3D(0,0,z0,c1);
-    [~,~,Bz2]=fieldCoil_3D(0,0,z0,c2);
-    [~,~,Bz3]=fieldCoil_3D(0,0,z0,c3);
-    [~,~,Bz4]=fieldCoil_3D(0,0,z0,c4);
-
-Bz1 = Bz1*1e4;
-Bz2= Bz2*1e4;
-Bz3 = Bz3*1e4;
-Bz4 = Bz4*1e4;
-
-    % Z Gradient
-    [~,~,Bz1p]=fieldCoil_3D(0,0,z0+dL,c1);
-    [~,~,Bz2p]=fieldCoil_3D(0,0,z0+dL,c2);
-    [~,~,Bz3p]=fieldCoil_3D(0,0,z0+dL,c3);
-    [~,~,Bz4p]=fieldCoil_3D(0,0,z0+dL,c4);
-    [~,~,Bz1n]=fieldCoil_3D(0,0,z0-dL,c1);
-    [~,~,Bz2n]=fieldCoil_3D(0,0,z0-dL,c2);
-    [~,~,Bz3n]=fieldCoil_3D(0,0,z0-dL,c3);
-    [~,~,Bz4n]=fieldCoil_3D(0,0,z0-dL,c4);
-
-    Gz1 = (Bz1p-Bz1n)/(2*dL);Gz1 = Gz1*1e2;
-    Gz2 = (Bz2p-Bz2n)/(2*dL);Gz2 = Gz2*1e2;
-    Gz3 = (Bz3p-Bz3n)/(2*dL);Gz3 = Gz3*1e2;
-    Gz4 = (Bz4p-Bz4n)/(2*dL);Gz4 = Gz4*1e2;
-    Bz(jj,:) = [Bz1 Bz2 Bz3 Bz4];
-    Gz(jj,:) = [Gz1 Gz2 Gz3 Gz4];
-
-    curr1 = i1(1) + (i2(1)-i1(1))/(zb-za)*(z0-za);
-    curr2 = i1(2) + (i2(2)-i1(2))/(zb-za)*(z0-za);
-    curr3 = i1(3) + (i2(3)-i1(3))/(zb-za)*(z0-za);
-    curr4 = i1(4) + (i2(4)-i1(4))/(zb-za)*(z0-za);
-
-
-    curr_lin(jj,:) = [curr1 curr2 curr3 curr4];
-end
-
-
-%%
-
-n1 = 1:length(zvec);
-n2 = (length(zvec)+1):(2*length(zvec));
-n3 = (2*length(zvec)+1):(3*length(zvec));
-n4 = (3*length(zvec)+1):(4*length(zvec));
-
-Ball = Bz(:);
-Gall = Gz(:);
-curr_lin_all = curr_lin(:);
-
-Gset = G*ones(length(zvec),1);
-Bset = zeros(length(zvec),1);
-
-% Magnetic Field Constraint
-A1 = zeros(length(zvec),4*length(zvec));
-B1 = zeros(length(zvec),1);
-for kk=1:length(zvec)
-    A1(kk,kk) = Bz(kk,1);
-    A1(kk,kk+length(zvec)) = Bz(kk,2);
-    A1(kk,kk+2*length(zvec)) = Bz(kk,3);
-    A1(kk,kk+3*length(zvec)) = Bz(kk,4);
-    B1(kk) = 0;
-end
-
-% Field Gradient Constraint
-A2 = zeros(length(zvec),4*length(zvec));
-B2 = zeros(length(zvec),1);
-for kk=1:length(zvec)
-    A2(kk,kk) = Gz(kk,1);
-    A2(kk,kk+length(zvec)) = Gz(kk,2);
-    A2(kk,kk+2*length(zvec)) = Gz(kk,3);
-    A2(kk,kk+3*length(zvec)) = Gz(kk,4);
-    B2(kk) = 100;
-end
-
-% Boundary Condition Constrain
-Abc = zeros(8,4*length(zvec));
-Bbc = zeros(8,1);
-
-Abc(1,1) = 1;Bbc(1) = i1(1);                
-Abc(2,1+length(zvec)) = 1;Bbc(2) = i1(2);
-Abc(3,1+2*length(zvec)) = 1;Bbc(3) = i1(3);
-Abc(4,1+3*length(zvec)) = 1;Bbc(4) = i1(4);
-Abc(5,length(zvec)) = 1;Bbc(5) = i2(1);                
-Abc(6,2*length(zvec)) = 1;Bbc(6) = i2(2);                
-Abc(7,3*length(zvec)) = 1;Bbc(7) = i2(3);                
-Abc(8,4*length(zvec)) = 1;Bbc(8) = i2(4);                
-
-
-A = [A1;A2;Abc];
-B = [B1;B2;Bbc];
-x0 = curr_lin_all;
-
-func = @(curr) sum(diff(curr(n1)).^2) + ...
-    sum(diff(curr(n2)).^2) + ...
-    sum(diff(curr(n3)).^2) + ...
-    sum(diff(curr(n4)).^2);
-
-x = fmincon(func,x0,[],[],A,B);
-
-Gfind = x(n1).*Gz(:,1)+x(n2).*Gz(:,2)+x(n3).*Gz(:,3)+x(n4).*Gz(:,4);
-Bfind = x(n1).*Bz(:,1)+x(n2).*Bz(:,2)+x(n3).*Bz(:,3)+x(n4).*Bz(:,4);
-%%
 figure(10);
 clf
-
-subplot(121);
-plot(zvec,x(n1));
+co=get(gca,'colororder');
+plot(zz*1e3,i1,'-','color',co(1,:),'linewidth',1);
 hold on
-plot(zvec,x(n2));
-plot(zvec,x(n3));
-plot(zvec,x(n4));
+plot(zz*1e3,i2,'-','color',co(2,:),'linewidth',1);
+plot(zz*1e3,i3,'-','color',co(3,:),'linewidth',1);
+plot(zz*1e3,i4,'-','color',co(4,:),'linewidth',1);
+plot(zz*1e3,i5,'-','color',co(5,:),'linewidth',1);
+plot(zz*1e3,i6,'-','color',co(6,:),'linewidth',1);
 
-subplot(122);
-plot(zvec,Gfind); 
-hold on
-plot(zvec,Bfind);
+Gsolve = interp1(Z,G(:,1),zz).*i1+interp1(Z,G(:,2),zz).*i2 + ...
+    interp1(Z,G(:,3),zz).*i3+interp1(Z,G(:,4),zz).*i4 + ...
+    interp1(Z,G(:,5),zz).*i5+interp1(Z,G(:,6),zz).*i6;
 
-%}
+Bsolve = interp1(Z,B(:,1),zz).*i1+interp1(Z,B(:,2),zz).*i2 + ...
+    interp1(Z,B(:,3),zz).*i3+interp1(Z,B(:,4),zz).*i4 + ...
+    interp1(Z,B(:,5),zz).*i5+interp1(Z,B(:,6),zz).*i6;
+
+legend({'12a','12b','13','14','15','16'},'orientation','horizontal',...
+    'location','southeast');
+
+Gstr = ['G = ' num2str(mean(Gsolve)) '\pm' num2str(std(Gsolve),3) ' G/cm'];
+
+Bstr = ['\DeltaB < ' num2str(range(Bsolve)) ' G'];
+
+str = [Gstr newline Bstr];
+% i1 = [i1_0a i1_ab]
+xlim([0 max(zz)*1e3]);
+ylim([-50 60]);
+xlabel('position (mm)');
+ylabel('current (A)');
+
+text(.01,.99,str,'units','normalized','verticalalignment','top');
+
+% yyaxis right
+% set(gca,'YColor','k');
+% ylabel('Gradient');
+% plot(zz,Gsolve);
+
+
 %% activate warning
 warning on
